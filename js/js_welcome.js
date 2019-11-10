83b5d940b2a5c413b295b3d9609a655e6e5ccf2e
Pio Nguyễn <50920220+Greeny167@users.noreply.github.com>
2019-11-10 12:13:43 +0700

    Nov-10-v1.1
    update thêm từ file của mnn

--- js/js_welcome.js
+++ js/js_welcome.js
@@ -1,5 +1,16 @@
 
 $(function(){
+	$('#login').on('click', function(e)
+	{
+		$('.row').addClass('zoomOutUp animated');
+	});
+
+	$('#close').on('click', function(e)
+	{
+		$('.row').removeClass('zoomOutUp animated');
+		$('.row').addClass('fadeInUp animated');
+	});
+
 	$('#btnSubmit').on('click', function(e)
 	{
 		var username = $('#username').val(),
@@ -8,12 +19,13 @@ $(function(){
 			correct_pass= 'admin';
 	 if (username === correct_user && pass === correct_pass)
 		  {
-	  		window.open('./test.html');
+		  	e.preventDefault();
+ 			window.location.href = './home.html';
 		  }
 	  else
 		  {
 		  	e.preventDefault();
-	 	 	swal("Something went wrong!");
+	 	 	swal("Error!", "Invalid password or username", "error").delay(1);
 		  }
 	});
 })
